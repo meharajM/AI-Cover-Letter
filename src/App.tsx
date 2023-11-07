1cae2419f4fcd9e02c99ce86ab32a6198877cbc2
meharaj <meharaj137@gmail.com>
2023-11-07 15:16:40 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <div className="text-3xl font-bold underline lg:text-5xl">
+        Hello world!
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
